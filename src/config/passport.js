4ef2f96420114b6bf05de7afbd65a9f599ae02c7
izzett222 <iizzeddin62@gmail.com>
2020-05-19 11:28:18 +0200

    bug(facebook): fix facebook login
    - modify the passport conig
    - add the env variable name to the .env.example
    
    [Maintains #172911727]

--- src/config/passport.js
+++ src/config/passport.js
@@ -6,27 +6,22 @@ import getProfile from '../utils/getProfile';
 import db from '../models';
 
 dotenv.config();
-
 passport.use(new GoogleStrategy({
-  callbackURL: '/api/v1/auth/google/redirect',
+  callbackURL: `${process.env.SERVER_API_URL}/api/v1/auth/google/redirect`,
   clientID: process.env.clientID,
   clientSecret: process.env.clientSecret,
 }, getProfile));
-
 passport.use(new FacebookStrategy({
-  callbackURL: '/api/v1/auth/facebook/redirect',
+  callbackURL: `${process.env.SERVER_API_URL}/api/v1/auth/facebook/redirect`,
   clientID: process.env.fbAppID,
   clientSecret: process.env.fbAppSecret,
   profileFields: ['id', 'name']
 }, getProfile));
-
 passport.serializeUser((user, done) => {
   done(null, user);
 });
-
 passport.deserializeUser(async (id, done) => {
   const user = await db.User.findOne({ where: { id } });
   done(null, user);
 });
-
 export default passport;
